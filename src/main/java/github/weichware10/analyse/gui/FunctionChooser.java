a402cbcaeccd3de16bad87846f45823e9fa3cb64
justinexner <92640370+justinexner@users.noreply.github.com>
2022-01-08 14:57:16 +0100

    [fix] Ergänzung zum vorherigen + Basic Function...

---
+++ src/main/java/github/weichware10/analyse/gui/FunctionChooser.java
@@ -0,0 +1,28 @@
+package github.weichware10.analyse.gui;
+
+import github.weichware10.analyse.Main;
+import github.weichware10.analyse.gui.util.AbsScene;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Parent;
+import javafx.stage.Screen;
+
+/**
+ * Der Auswahlbildschirm der App.
+ */
+public class FunctionChooser extends AbsScene {
+    private static Parent root;
+
+    /**
+     * Startet die App.
+     */
+    public static void start() {
+        Rectangle2D screenBounds = Screen.getPrimary().getBounds();
+        root = start(Main.primaryStage,
+                Login.class.getResource("FunctionChooser.fxml"),
+                root,
+                "Analyse",
+                MainMenuBar.getMenuBar(),
+                (int) screenBounds.getWidth() / 2,
+                (int) screenBounds.getHeight() / 2);
+    }
+}
---
+++ src/main/java/github/weichware10/analyse/gui/FunctionChooserController.java
@@ -0,0 +1,14 @@
+package github.weichware10.analyse.gui;
+
+import github.weichware10.analyse.gui.util.AbsSceneController;
+
+/**
+ * Controller für {@link FunctionChooser}.
+ */
+public class FunctionChooserController extends AbsSceneController {
+
+    @Override
+    protected void initialize() {
+        ;
+    }
+}
