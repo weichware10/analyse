09f7a96de649b40109b8fabb8a431a650fad0f0b
joshuajeschek <joshua.jeschek@gmail.com>
2022-01-15 12:56:54 +0100

    [fix] Fenstergröße resized nur bei Login

--- src/main/java/github/weichware10/analyse/gui/general/FunctionChooser.java
+++ src/main/java/github/weichware10/analyse/gui/general/FunctionChooser.java
@@ -2,9 +2,7 @@ package github.weichware10.analyse.gui.general;
 
 import github.weichware10.analyse.Main;
 import github.weichware10.analyse.gui.util.AbsScene;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
-import javafx.stage.Screen;
 
 /**
  * Der Auswahlbildschirm der App.
@@ -16,14 +14,13 @@ public class FunctionChooser extends AbsScene {
      * Startet die App.
      */
     public static void start() {
-        Rectangle2D screenBounds = Screen.getPrimary().getBounds();
         root = start(Main.primaryStage,
                 Login.class.getResource("FunctionChooser.fxml"),
                 root,
                 null,
                 "Analyse",
                 MainMenuBar.getMenuBar(),
-                (int) screenBounds.getWidth() / 2,
-                (int) screenBounds.getHeight() / 2).root;
+                null,
+                null).root;
     }
 }
