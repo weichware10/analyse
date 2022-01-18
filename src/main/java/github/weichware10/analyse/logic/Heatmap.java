349c209e5a442a50c3cff8b64adc005c6c8e482f
joshuajeschek <joshua.jeschek@gmail.com>
2022-01-18 17:36:40 +0100

    [rework] heatmap

--- src/main/java/github/weichware10/analyse/logic/Heatmap.java
+++ src/main/java/github/weichware10/analyse/logic/Heatmap.java
@@ -4,21 +4,26 @@ import github.weichware10.analyse.Main;
 import github.weichware10.analyse.config.HeatmapConfig;
 import github.weichware10.util.Files;
 import github.weichware10.util.Logger;
-import github.weichware10.util.ToolType;
 import github.weichware10.util.config.Configuration;
 import github.weichware10.util.data.DataPoint;
 import github.weichware10.util.data.TrialData;
+import java.awt.AlphaComposite;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import javafx.scene.image.Image;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 import javax.imageio.ImageIO;
 
 /**
  * verantwortlich für die Erstellung der Heatmap.
  */
 public class Heatmap {
+
+    private static DataPointComparator comparator = new DataPointComparator();
+
 
     /**
      * Erstellt Heatmap.
@@ -31,10 +36,10 @@ public class Heatmap {
         Configuration config = Main.dataBaseClient.configurations.get(trial.configId);
 
         // Bild laden für Bildbreite und -höhe
-        String imageUrl = Analyse.saveImage(config.getImageUrl());
-        Image image = null;
+        String sourceImgLocation = Analyse.saveImage(config.getImageUrl());
+        BufferedImage image = null;
         try {
-            image = new Image(imageUrl);
+            image = ImageIO.read(new File(sourceImgLocation));
         } catch (Exception e) {
             Logger.error("Failed to save the image", e, true);
             return null;
@@ -44,84 +49,41 @@ public class Heatmap {
 
         // Heatmap Bild erstellen
         BufferedImage heatmap = null;
-        double opacity;
 
-        // Wenn Versuchs-Bild angezeigt werden soll, dieses als Hintergrund setzen sonst
-        // leerer Hintergrund
-        if (hmConfig.isImage()) {
-            try {
-                heatmap = ImageIO.read(new File(imageUrl));
-            } catch (IOException e) {
-                Logger.error("Failed to load image", e, true);
-            }
-            opacity = 0.5f;
-        } else {
-            heatmap = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-            opacity = 1.0f;
-        }
+        heatmap = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D heatmapGraphics = heatmap.createGraphics();
+        heatmapGraphics.setBackground(new java.awt.Color(1, 1, 1, 0));
+        heatmapGraphics.clearRect(0, 0, width, height);
 
-        Graphics2D graphic = heatmap.createGraphics();
+        // so sortieren, dass immer weiter reingezoomt wird
+        List<DataPoint> sortedDataPoints = trial.getData().stream()
+                .sorted(comparator).collect(Collectors.toList());
 
-        if (trial.toolType == ToolType.ZOOMMAPS) {
-            // Tiefe 0 setzten
-            graphic.setColor(calcColorCc(0.0f, opacity,
-                    hmConfig.getMinColorDiff(), hmConfig.getMaxColorDiff()));
-            graphic.fillRect(0, 0, width, height);
-
-            // Maximale Tiefe ermitteln
-            double maxDepth = Analyse.findMaxDepth(
-                    trial.getData(), (double) width, (double) height);
-            double oldRelDepth = 0.0f;
-
-            // Rechtecke der Zooms setzen
-            for (DataPoint dataPoint : trial.getData()) {
-                // Relative Tiefe berechnen
-                double relDepth = Analyse.calcRelDepthZm(dataPoint.viewport, width,
-                        height, maxDepth);
-
-                // Prüfen ob herausgezoomt wurde
-                if (oldRelDepth > relDepth) {
-                    oldRelDepth = relDepth;
-                    continue;
-                }
-                oldRelDepth = relDepth;
-
-                // Rechteck färben und setzten
-                graphic.setColor(calcColorZm(relDepth, 0.1f, hmConfig.getMaxColorDiff()));
-                int minX = (dataPoint.viewport.getMinX() < 0.0f
-                        ? 0 : (int) dataPoint.viewport.getMinX());
-                int minY = (dataPoint.viewport.getMinY() < 0.0f
-                        ? 0 : (int) dataPoint.viewport.getMinY());
-                graphic.fillRect(minX, minY, (int) dataPoint.viewport.getWidth(),
-                        (int) dataPoint.viewport.getHeight());
-            }
-        } else if (trial.toolType == ToolType.CODECHARTS) {
-            // Tiefe 0 setzten
-            graphic.setColor(calcColorCc(0.0f, opacity,
-                    hmConfig.getMinColorDiff(), hmConfig.getMaxColorDiff()));
-            graphic.fillRect(0, 0, width, height);
-
-            // Rechtecke der Durchläufe setzen
-            for (DataPoint dataPoint : trial.getData()) {
-                // Relative Tiefe berechnen
-                double relDepth = Analyse.calcRelDepthCc((double) dataPoint.depth,
-                        (double) config.getCodeChartsConfiguration().getMaxDepth());
-
-                // Rechteck färben und setzten
-                graphic.setColor(calcColorCc(relDepth, opacity,
-                        hmConfig.getMinColorDiff(), hmConfig.getMaxColorDiff()));
-                graphic.fillRect((int) dataPoint.viewport.getMinX(),
-                        (int) dataPoint.viewport.getMinY(),
-                        (int) dataPoint.viewport.getWidth(),
-                        (int) dataPoint.viewport.getHeight());
-            }
-        }
-        graphic.dispose();
+        createHeatmapFromData(heatmapGraphics, hmConfig, sortedDataPoints, width, height);
+        heatmapGraphics.dispose();
 
         // Generierte Heatmap-Bild speichern
+        String heatmapLocation = null;
+        try {
+            heatmapLocation = Files.saveGeneratedImage(heatmap, "HEATMAP" + trial.trialId + ".png");
+        } catch (IllegalArgumentException | IOException e) {
+            Logger.error("Failed to save the image", e, true);
+        }
+
+        if (image == null || !hmConfig.isImage()) {
+            return heatmapLocation;
+        }
+
+        Graphics2D imageGraphics = image.createGraphics();
+        AlphaComposite acomp = AlphaComposite.getInstance(
+                AlphaComposite.SRC_OVER, .5f);
+        imageGraphics.setComposite(acomp);
+        imageGraphics.drawImage(heatmap, 0, 0, null);
+        imageGraphics.dispose();
+
         String imgLocation = null;
         try {
-            imgLocation = Files.saveGeneratedImage(heatmap, "test.png");
+            imgLocation = Files.saveGeneratedImage(image, "IMGHEATMAP" + trial.trialId + ".png");
         } catch (IllegalArgumentException | IOException e) {
             Logger.error("Failed to save the image", e, true);
         }
@@ -129,45 +91,33 @@ public class Heatmap {
         return imgLocation;
     }
 
-    /**
-     * Berechnet neue Farbe anhand der Tiefe (CodeCharts).
-     *
-     * @param relDepth     - relative Tiefe
-     * @param opacity      - Transparenz
-     * @param minColorDiff - minimale Farbe
-     * @param maxColorDiff - maximale Farbe
-     * @return berechnete Farbe (java.awt.Color Color)
-     */
-    private static java.awt.Color calcColorCc(double relDepth, double opacity,
-            javafx.scene.paint.Color minColorDiff,
-            javafx.scene.paint.Color maxColorDiff) {
-        double r = minColorDiff.getRed() * (1.0f - relDepth) + maxColorDiff.getRed() * relDepth;
-        double g = minColorDiff.getGreen() * (1.0f - relDepth) + maxColorDiff.getGreen() * relDepth;
-        double b = minColorDiff.getBlue() * (1.0f - relDepth) + maxColorDiff.getBlue() * relDepth;
-        // Logger.debug(String.format("R:%f, G:%fm B:%f", r, g, b));
+    private static void createHeatmapFromData(Graphics2D graphic, HeatmapConfig hmConfig,
+            List<DataPoint> sortedDataPoints, double imageWidth, double imageHeight) {
 
-        javafx.scene.paint.Color color = new javafx.scene.paint.Color(r, g, b, opacity);
-        return HeatmapConfig.fxToAwtColor(color);
-    }
+        if (sortedDataPoints.size() == 0) {
+            return;
+        }
 
-    /**
-     * Berechnet neue Farbe anhand der Tiefe (ZoomMaps).
-     *
-     * @param relDepth     - relative Tiefe
-     * @param opacity      - Transparenz
-     * @param maxColorDiff - maximale Farbe
-     * @return berechnete Farbe (java.awt.Color Color)
-     */
-    private static java.awt.Color calcColorZm(double relDepth, double opacity,
-            javafx.scene.paint.Color maxColorDiff) {
-        double r = 1.0f * (1.0f - relDepth) + maxColorDiff.getRed() * relDepth;
-        double g = 1.0f * (1.0f - relDepth) + maxColorDiff.getGreen() * relDepth;
-        double b = 1.0f * (1.0f - relDepth) + maxColorDiff.getBlue() * relDepth;
-        double a = opacity * relDepth;
-        // Logger.debug(String.format("R:%f, G:%fm B:%f", r, g, b));
+        DataPoint minDataPoint = sortedDataPoints.get(0);
+        DataPoint maxDataPoint = sortedDataPoints.get(sortedDataPoints.size() - 1);
 
-        javafx.scene.paint.Color color = new javafx.scene.paint.Color(r, g, b, a);
-        return HeatmapConfig.fxToAwtColor(color);
+        double minDepth = Analyse.calculateDepth(minDataPoint, imageWidth, imageHeight, null, null);
+        double maxDepth = Analyse.calculateDepth(maxDataPoint, imageWidth, imageHeight, null, null);
+
+        Logger.debug(Double.toString(minDepth) + " ... " + Double.toString(maxDepth));
+
+        for (DataPoint dataPoint : sortedDataPoints) {
+            double relDepth = Analyse.calculateDepth(
+                    dataPoint, imageWidth, imageHeight, minDepth, maxDepth);
+            Logger.debug(Double.toString(relDepth));
+            graphic.setColor(HeatmapConfig.fxToAwtColor(hmConfig.getMinColorDiff()
+                    .interpolate(hmConfig.getMaxColorDiff(), relDepth)));
+            graphic.fillRect(
+                    (int) dataPoint.viewport.getMinX(),
+                    (int) dataPoint.viewport.getMinY(),
+                    (int) dataPoint.viewport.getWidth(),
+                    (int) dataPoint.viewport.getHeight());
+        }
     }
 
     /**
@@ -181,5 +131,18 @@ public class Heatmap {
     public static String createHeatmapComp(TrialData trial,
             TrialData trialComp, HeatmapConfig hmConfig) {
         return null;
+    }
+
+    /**
+     * DataPointComparator.
+     */
+    private static class DataPointComparator implements Comparator<DataPoint> {
+
+        @Override
+        public int compare(DataPoint dp1, DataPoint dp2) {
+            int area1 = (int) (dp1.viewport.getWidth() * dp1.viewport.getHeight());
+            int area2 = (int) (dp2.viewport.getWidth() * dp2.viewport.getHeight());
+            return area2 - area1;
+        }
     }
 }
